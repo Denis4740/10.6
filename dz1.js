a14583bb2bbcb1114110844cf6612e77767907c0
Denis4740 <den.lobov.07@bk.ru>
2025-06-16 00:48:17 +0300

    Update dz1.js

---
+++ dz1.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+    console.log('Страница загрузилась')
+});
+const button = document.querySelector('button');
+button.onclick = ()=> {
+    console.log('событие onclick');
+}
+button.addEventListener('blur', ()=>{
+    console.log('событие addEventListener');
+});
