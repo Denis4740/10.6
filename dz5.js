cef65482dbb9869c1298ef0c15df667a366ac1ff
Denis4740 <den.lobov.07@bk.ru>
2025-06-16 00:59:29 +0300

    Update dz5.js

---
+++ dz5.js
@@ -0,0 +1,6 @@
+const button = document.createElement('button')
+button.textContent='Отправить';
+document.body.appendChild(button);
+button.onclick = function(){
+    this.textContent='Текст отправлен';
+}
